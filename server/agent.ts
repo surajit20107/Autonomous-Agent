34560276f289d892bdbdebcafbc6ecc211017255
Surajit <surajitjana20107@gmail.com>
2026-03-08 06:35:46 +0000

    create a basic todo fetching tool for ai

--- server/agent.ts
+++ server/agent.ts
@@ -1,12 +1,28 @@
 import "dotenv/config";
 import { createAgent } from "langchain";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { tool } from "langchain";
+import { z } from "zod";
 
 const api_key = process.env.GEMINI_API_KEY;
 
 if (!api_key) {
   throw new Error("GEMINI_API_KEY is not defined");
 }
+
+const get_todos = tool(
+  async ({ query }: { query: string }) => {
+    const todos = await fetch(`https://jsonplaceholder.typicode.com/todos/${query}`)
+    const data = await todos.json()
+    return data
+  }, {
+    name: "get_todos",
+    description: "get todos from the api",
+    schema: z.object({
+      query: z.string().describe("the todo id to fetch")
+    })
+  }
+)
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
@@ -15,16 +31,16 @@ const model = new ChatGoogleGenerativeAI({
 
 const agent = createAgent({
   model,
-  tools: [],
+  tools: [get_todos],
 });
 
 const result = await agent.invoke({
   messages: [
     {
       role: "user",
-      content: "What is the capital of France?",
+      content: "tell me the todo with id 4",
     },
   ],
 });
 
-console.log(result?.messages?.[1]?.content);
+console.log(result?.messages?.[result.messages.length - 1]?.content);
